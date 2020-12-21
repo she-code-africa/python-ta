5f4c231379dec3f276638ce09465480e4ecea769
Josh Ike <mail@chukwuanu.com>
2020-12-21 12:54:27 +0100

    update testcase

--- src/app.py
+++ src/app.py
@@ -2,27 +2,15 @@ import sys
 import os
 
 
-def fibonacci(n):
-    r = list()
+def palindrome(s):
+    # your code goes here
 
-    for i in range(n):
-        if i == 0 or i == 1:
-            r.insert(i, i)
-        else:
-            v = r[i - 1] + r[i - 2]
-            r.insert(i, v)
-
-    return r
-
-
-def solution(n):
-    # Solution
-    return fibonacci(n)
+def solution(s):
+    return palindrome(s)
 
 
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
         sys.exit(os.error("Argment required"))
 
-    n = int(sys.argv[1])
-    print(solution(n))
+    print(solution(sys.argv[1]))
