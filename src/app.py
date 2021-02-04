9e325f974be300da49c3475ab6a366d461b64844
Oluwatomi01 <78262830+Oluwatomi01@users.noreply.github.com>
2021-02-04 22:31:06 +0100

    sca project

--- src/app.py
+++ src/app.py
@@ -4,7 +4,20 @@ import os
 
 def palindrome(s):
     # your code goes here
-
+    source = []
+    for letter in s:
+        letter = letter.strip()
+        letter = letter.lower()
+        if len(letter) <= 0:
+            continue
+        source.append(letter)
+    account = (len(source) - 1)
+    for entry in source:
+        if entry == source[account]:
+            account -= 1
+            continue
+    if account == -1:
+        print(s)
 def solution(s):
     return palindrome(s)
 
